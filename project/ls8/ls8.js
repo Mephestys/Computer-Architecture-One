31d899235deff644c688068b526b390c19ca1c62
Mephestys <mephestys@gmail.com>
2018-04-09 16:50:14 -0600

    Added some comments to `program` in ls8.js - Clears up what each line is actually doing.

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -12,10 +12,10 @@ function loadMemory() {
 
   const program = [ // print8.ls8
     "10011001", // LDI R0,8  Store 8 into R0
-    "00000000",
-    "00001000",
+    "00000000", // R0         OperandA for LDI instruction
+    "00001000", // 8          OperandB for LDI instruction
     "01000011", // PRN R0    Print the value in R0
-    "00000000",
+    "00000000", // R0         OperandA for PRN instruction
     "00000001"  // HLT       Halt and quit
   ];
 
